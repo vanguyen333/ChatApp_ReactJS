e0b8af647c7339c5c58fca82330ffc6ce752902f
vanguyen333 <96743036+vanguyen333@users.noreply.github.com>
2022-12-31 21:02:29 -0500

    random quote generated on h1

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+
+import "./App.css";
+
+class App extends React.Component {
+  state = { advice: "" };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return <h1>{advice}</h1>;
+  }
+}
+
+export default App;
